28b20b0c15b5be1d9f8ebcc349d194821fcdccbb
Philipp <ep2277@mci4me.at>
2024-04-16 17:43:41 +0200

    Create new class in dev_hackrf.py Insert File ieee802_15_4_oqpsk_phy.py Insert File transceiver_OQPSK_sniffer.py

--- killerbee/dev_hackrf.py
+++ killerbee/dev_hackrf.py
@@ -1,1 +1,312 @@
 
+
+from typing import Optional, Dict, Union, Any, List
+
+import os
+import time
+from datetime import datetime, timedelta
+from .kbutils import KBCapabilities, makeFCS
+from .GoodFETCCSPI import GoodFETCCSPI
+from libhackrf import *
+
+
+'Chipcon CC2420 Register Definitions, means to be moved to kbutils.py or similar'
+CC2420_REG_SYNC: int = 0x14
+
+
+class HackRF:
+    def __init__(self, dev: str) -> None:
+        '''
+        Instantiates the KillerBee class for the HackRF platform running GoodFET firmware.
+        @type dev:   String
+        @param dev:  Serial device identifier (ex /dev/ttyUSB0)
+        @return: None
+        @rtype: None
+        '''
+        'self.packet_queue is used to store a packet when pnext() is called and a packet is already available.'
+        self.packet_queue: Optional[bytes] = None
+        self.packet_queue_rssi: Optional[int] = None
+
+        self._channel: Optional[int] = None
+        self._page: int = 0
+        self.handle: Optional[Any] = None
+        self.dev: str = dev
+
+        # Set environment variables for GoodFET code to use
+        os.environ["platform"] = "apimote2"
+        os.environ["board"] = "apimote2"
+
+        self.handle = HackRF()
+        self.handle.serInit(port=self.dev)
+        self.handle.setup()
+
+        self.__stream_open: bool = False
+        self.capabilities: KBCapabilities = KBCapabilities()
+        self.__set_capabilities()
+
+    def close(self) -> None:
+
+        if self.handle is None:
+            raise Exception("Handle does not exist");
+
+        self.handle.serClose()
+        self.handle = None
+
+    def check_capability(self, capab: int) -> bool:
+        return self.capabilities.check(capab)
+
+    def get_capabilities(self) -> Dict[int, bool]:
+        return self.capabilities.getlist()
+
+    def __set_capabilities(self) -> None:
+        '''
+        Sets the capability information appropriate for GoodFETCCSPI client and firmware.
+        @rtype: None
+        @return: None
+        '''
+        self.capabilities.setcapab(KBCapabilities.FREQ_2400, True)
+        self.capabilities.setcapab(KBCapabilities.SNIFF, True)
+        self.capabilities.setcapab(KBCapabilities.SETCHAN, True)
+        self.capabilities.setcapab(KBCapabilities.INJECT, True)
+        self.capabilities.setcapab(KBCapabilities.PHYJAM_REFLEX, True)
+        self.capabilities.setcapab(KBCapabilities.SET_SYNC, True)
+        self.capabilities.setcapab(KBCapabilities.PHYJAM, True)
+        return
+
+    # KillerBee expects the driver to implement this function
+    def get_dev_info(self) -> List[Union[str, Any]]:
+        '''
+        Returns device information in a list identifying the device.
+        @rtype: List
+        @return: List of 3 strings identifying device.
+        '''
+        return [self.dev, "GoodFET Apimote v2", ""]
+
+    # KillerBee expects the driver to implement this function
+    def sniffer_on(self, channel: Optional[int] = None, page: int = 0) -> None:
+        '''
+        Turns the sniffer on such that pnext() will start returning observed
+        data.  Will set the command mode to Air Capture if it is not already
+        set.
+        @type channel: Integer
+        @param channel: Sets the channel, optional
+        @type page: Integer
+        @param page: Sets the subghz page, not supported on this device
+        @rtype: None
+        '''
+        self.capabilities.require(KBCapabilities.SNIFF)
+
+        if self.handle is None:
+            raise Exception("Handle does not exist")
+
+        self.handle.RF_promiscuity(1)
+        self.handle.RF_autocrc(0)
+
+        if channel is not None:
+            self.set_channel(channel, page)
+
+        self.handle.CC_RFST_RX()
+
+        self.__stream_open = True
+
+    # KillerBee expects the driver to implement this function
+    def sniffer_off(self) -> None:
+        '''
+        Turns the sniffer off, freeing the hardware for other functions.  It is
+        not necessary to call this function before closing the interface with
+        close().
+        @rtype: None
+        '''
+        # TODO actually have firmware stop sending us packets!
+        self.__stream_open = False
+
+    # KillerBee expects the driver to implement this function
+    def set_channel(self, channel: int, page: int = 0) -> None:
+        '''
+        Sets the radio interface to the specifid channel (limited to 2.4 GHz channels 11-26)
+        @type channel: Integer
+        @param channel: Sets the channel, optional
+        @rtype: None
+        '''
+        if self.handle is None:
+            raise Exception("Handle does not exist")
+
+        self.capabilities.require(KBCapabilities.SETCHAN)
+
+        if channel >= 11 and channel <= 26:
+            self._channel = channel
+            self.handle.RF_setchan(channel)
+        else:
+            raise Exception('Invalid channel')
+        if page:
+            raise Exception('SubGHz not supported')
+
+    # KillerBee expects the driver to implement this function
+    def inject(self, packet: bytes, channel: Optional[int] = None, count: int = 1, delay: int = 0,
+               page: int = 0) -> None:
+        '''
+        Injects the specified packet contents.
+        @type packet: Bytes
+        @param packet: Packet contents to transmit, without FCS.
+        @type channel: Integer
+        @param channel: Sets the channel, optional
+        @type page: Integer
+        @param page: Sets the subghz page, not supported on this device
+        @type count: Integer
+        @param count: Transmits a specified number of frames, def=1
+        @type delay: Float
+        @param delay: Delay between each frame, def=1
+        @rtype: None
+        '''
+        if self.handle is None:
+            raise Exception("Handle does not exist")
+
+        self.capabilities.require(KBCapabilities.INJECT)
+
+        if len(packet) < 1:
+            raise Exception('Empty packet')
+        if len(packet) > 125:  # 127 - 2 to accommodate FCS
+            raise Exception('Packet too long')
+
+        if channel is not None:
+            self.set_channel(channel, page)
+
+        self.handle.RF_autocrc(1)  # let radio add the CRC
+        for pnum in range(0, count):
+            gfready = list(bytearray(packet))  # convert packet string to GoodFET expected integer format
+            gfready.insert(0, len(gfready) + 2)  # add a length that leaves room for CRC
+            self.handle.RF_txpacket(gfready)
+            time.sleep(1)
+
+    # KillerBee expects the driver to implement this function
+    def pnext(self, timeout: int = 100) -> Any:
+        '''
+        Returns a dictionary containing packet data, else None.
+        @type timeout: Integer
+        @param timeout: Timeout to wait for packet reception in usec
+        @rtype: List
+        @return: Returns None is timeout expires and no packet received.  When a packet is received, a dictionary is returned with the keys bytes (string of packet bytes), validcrc (boolean if a vaid CRC), rssi (unscaled RSSI), and location (may be set to None). For backwards compatibility, keys for 0,1,2 are provided such that it can be treated as if a list is returned, in the form [ String: packet contents | Bool: Valid CRC | Int: Unscaled RSSI ]
+        '''
+
+        if self.handle is None:
+            raise Exception("Handle does not exist")
+
+        if self.__stream_open == False:
+            self.sniffer_on()
+
+        if self.packet_queue is None:
+            packet: Optional[bytes] = None
+            start = datetime.now()
+
+            while (packet is None) and ((start + timedelta(seconds=timeout)) > datetime.now()):
+                packet = self.handle.RF_rxpacket()
+                rssi = self.handle.RF_getrssi()  # TODO calibrate
+
+            if packet is None:
+                return None
+
+            if packet[0] + 1 < len(packet):
+                self.packet_queue = packet[packet[0] + 1 + 1:]
+                self.packet_queue_rssi = rssi
+
+            frame = packet[1:packet[0] + 1]
+
+        else:
+            packet = self.packet_queue
+            self.packet_queue = None
+            frame = packet
+
+            if self.packet_queue_rssi is None:
+                rssi = None
+            else:
+                rssi = self.packet_queue_rssi
+
+        validcrc: bool = False
+        if frame[-2:] == makeFCS(frame[:-2]):
+            validcrc = True
+        # Return in a nicer dictionary format, so we don't have to reference by number indicies.
+        # Note that 0,1,2 indicies inserted twice for backwards compatibility.
+        result: Dict[Union[int, str], Any] = {
+            0: frame,
+            1: validcrc,
+            2: rssi,
+            'bytes': frame,
+            'validcrc': validcrc,
+            'rssi': rssi,
+            'location': None
+        }
+
+        result['datetime'] = datetime.utcnow()
+        if rssi is None:
+            result['dbm'] = None
+        else:
+            result[
+                'dbm'] = rssi - 45  # TODO tune specifically to the Apimote platform (does ext antenna need to different?)
+        return result
+
+    def ping(self, da: Any, panid: Any, sa: Any, channel: Optional[int] = None, page: int = 0) -> None:
+        '''
+        Not yet implemented.
+        @return: None
+        @rtype: None
+        '''
+        raise Exception('Not yet implemented')
+
+    def jammer_on(self, channel: Optional[int] = None, page: int = 0, method: Optional[str] = None) -> None:
+        '''
+        Implements reflexive jamming or constant carrier wave jamming.
+        @type channel: Integer
+        @param channel: Sets the channel, optional
+        @type page: Integer
+        @param page: Sets the subghz page, not supported on this device
+        @rtype: None
+        '''
+
+        if self.handle is None:
+            raise Exception("Handle does not exist")
+
+        if method is None:
+            method = "constant"
+
+        if method == "reflexive":
+            self.capabilities.require(KBCapabilities.PHYJAM_REFLEX)
+        elif method == "constant":
+            self.capabilities.require(KBCapabilities.PHYJAM)
+        else:
+            raise ValueError('Parameter "method" must be either \'reflexive\' or \'constant\'.')
+
+        self.handle.RF_promiscuity(1)
+        self.handle.RF_autocrc(0)
+
+        if channel is not None:
+            self.set_channel(channel, page)
+
+        self.handle.CC_RFST_RX()
+
+        if method == "reflexive":
+            self.handle.RF_reflexjam()
+        elif method == 'constant':
+            self.handle.RF_carrier()
+
+            # TODO maybe move sync to byte string rather than int
+
+    def set_sync(self, sync: int = 0xA70F) -> Any:
+        '''Set the register controlling the 802.15.4 PHY sync byte.'''
+        if self.handle is None:
+            raise Exception("Handle does not exist")
+
+        self.capabilities.require(KBCapabilities.SET_SYNC)
+        if (sync >> 16) > 0:
+            raise Exception("Sync word (%x) must be 2-bytes or less." % sync)
+        return self.handle.poke(CC2420_REG_SYNC, sync)
+
+    def jammer_off(self) -> None:
+        '''
+        Not yet implemented.
+        @return: None
+        @rtype: None
+        '''
+        # TODO implement
+        raise Exception('Not yet implemented')
+
+
---
+++ killerbee/transceiver_OQPSK_sniffer.py
@@ -0,0 +1,291 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: IEEE 802.15.4 Transceiver using OQPSK PHY
+# GNU Radio version: 3.10.1.1
+
+from packaging.version import Version as StrictVersion
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print("Warning: failed to XInitThreads()")
+
+import os
+import sys
+sys.path.append(os.environ.get('GRC_HIER_PATH', os.path.expanduser('~/.grc_gnuradio')))
+
+from PyQt5 import Qt
+from gnuradio import qtgui
+from gnuradio.filter import firdes
+import sip
+from gnuradio import blocks
+from gnuradio import gr
+from gnuradio.fft import window
+import signal
+from argparse import ArgumentParser
+from gnuradio import eng_notation
+from gnuradio.qtgui import Range, RangeWidget
+from PyQt5 import QtCore
+from ieee802_15_4_oqpsk_phy import ieee802_15_4_oqpsk_phy  # grc-generated hier_block
+import foo
+import ieee802_15_4
+import osmosdr
+import time
+
+
+
+from gnuradio import qtgui
+
+class transceiver_OQPSK_sniffer(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "IEEE 802.15.4 Transceiver using OQPSK PHY", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("IEEE 802.15.4 Transceiver using OQPSK PHY")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "transceiver_OQPSK_sniffer")
+
+        try:
+            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+                self.restoreGeometry(self.settings.value("geometry").toByteArray())
+            else:
+                self.restoreGeometry(self.settings.value("geometry"))
+        except:
+            pass
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.tx_gain = tx_gain = 0.75
+        self.rx_gain = rx_gain = 0.75
+        self.freq = freq = 2480000000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        # Create the options list
+        self._freq_options = [2405000000, 2410000000, 2415000000, 2420000000, 2425000000, 2430000000, 2435000000, 2440000000, 2445000000, 2450000000, 2455000000, 2460000000, 2465000000, 2470000000, 2475000000, 2480000000]
+        # Create the labels list
+        self._freq_labels = ['11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26']
+        # Create the combo box
+        self._freq_tool_bar = Qt.QToolBar(self)
+        self._freq_tool_bar.addWidget(Qt.QLabel("Channel" + ": "))
+        self._freq_combo_box = Qt.QComboBox()
+        self._freq_tool_bar.addWidget(self._freq_combo_box)
+        for _label in self._freq_labels: self._freq_combo_box.addItem(_label)
+        self._freq_callback = lambda i: Qt.QMetaObject.invokeMethod(self._freq_combo_box, "setCurrentIndex", Qt.Q_ARG("int", self._freq_options.index(i)))
+        self._freq_callback(self.freq)
+        self._freq_combo_box.currentIndexChanged.connect(
+            lambda i: self.set_freq(self._freq_options[i]))
+        # Create the radio buttons
+        self.top_layout.addWidget(self._freq_tool_bar)
+        self._tx_gain_range = Range(0, 1, 0.01, 0.75, 200)
+        self._tx_gain_win = RangeWidget(self._tx_gain_range, self.set_tx_gain, "'tx_gain'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._tx_gain_win)
+        self._rx_gain_range = Range(0, 1, 0.01, 0.75, 200)
+        self._rx_gain_win = RangeWidget(self._rx_gain_range, self.set_rx_gain, "'rx_gain'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._rx_gain_win)
+        self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_c(
+            1024, #size
+            window.WIN_BLACKMAN_hARRIS, #wintype
+            0, #fc
+            4000000, #bw
+            "", #name
+            1, #number of inputs
+            None # parent
+        )
+        self.qtgui_waterfall_sink_x_0.set_update_time(0.10)
+        self.qtgui_waterfall_sink_x_0.enable_grid(False)
+        self.qtgui_waterfall_sink_x_0.enable_axis_labels(True)
+
+
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        colors = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_waterfall_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_waterfall_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_waterfall_sink_x_0.set_color_map(i, colors[i])
+            self.qtgui_waterfall_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self.qtgui_waterfall_sink_x_0.set_intensity_range(-140, 10)
+
+        self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.qwidget(), Qt.QWidget)
+
+        self.top_layout.addWidget(self._qtgui_waterfall_sink_x_0_win)
+        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
+            1024, #size
+            window.WIN_BLACKMAN_hARRIS, #wintype
+            0, #fc
+            4e6, #bw
+            "", #name
+            1,
+            None # parent
+        )
+        self.qtgui_freq_sink_x_0.set_update_time(0.10)
+        self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
+        self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
+        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
+        self.qtgui_freq_sink_x_0.enable_autoscale(False)
+        self.qtgui_freq_sink_x_0.enable_grid(False)
+        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
+        self.qtgui_freq_sink_x_0.enable_axis_labels(True)
+        self.qtgui_freq_sink_x_0.enable_control_panel(False)
+        self.qtgui_freq_sink_x_0.set_fft_window_normalized(False)
+
+
+
+        labels = ['', '', '', '', '',
+            '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+            "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self.osmosdr_source_0 = osmosdr.source(
+            args="numchan=" + str(1) + " " + 'hackrf=0'
+        )
+        self.osmosdr_source_0.set_time_unknown_pps(osmosdr.time_spec_t())
+        self.osmosdr_source_0.set_sample_rate(4000000)
+        self.osmosdr_source_0.set_center_freq(freq, 0)
+        self.osmosdr_source_0.set_freq_corr(0, 0)
+        self.osmosdr_source_0.set_dc_offset_mode(0, 0)
+        self.osmosdr_source_0.set_iq_balance_mode(0, 0)
+        self.osmosdr_source_0.set_gain_mode(False, 0)
+        self.osmosdr_source_0.set_gain(10, 0)
+        self.osmosdr_source_0.set_if_gain(20, 0)
+        self.osmosdr_source_0.set_bb_gain(20, 0)
+        self.osmosdr_source_0.set_antenna('', 0)
+        self.osmosdr_source_0.set_bandwidth(0, 0)
+        self.ieee802_15_4_oqpsk_phy_0 = ieee802_15_4_oqpsk_phy()
+        self.ieee802_15_4_mac_0 = ieee802_15_4.mac(True,0x8841,0,0x1aaa,0xffff,0x3344)
+        self.foo_wireshark_connector_0 = foo.wireshark_connector(195, False)
+        self.blocks_file_sink_1 = blocks.file_sink(gr.sizeof_gr_complex*1, '/dev/null', True)
+        self.blocks_file_sink_1.set_unbuffered(False)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, '/tmp/zigbee.pcap', True)
+        self.blocks_file_sink_0.set_unbuffered(True)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.msg_connect((self.ieee802_15_4_mac_0, 'pdu out'), (self.foo_wireshark_connector_0, 'in'))
+        self.msg_connect((self.ieee802_15_4_mac_0, 'pdu out'), (self.ieee802_15_4_oqpsk_phy_0, 'txin'))
+        self.msg_connect((self.ieee802_15_4_oqpsk_phy_0, 'rxout'), (self.foo_wireshark_connector_0, 'in'))
+        self.msg_connect((self.ieee802_15_4_oqpsk_phy_0, 'rxout'), (self.ieee802_15_4_mac_0, 'pdu in'))
+        self.connect((self.foo_wireshark_connector_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.ieee802_15_4_oqpsk_phy_0, 0), (self.blocks_file_sink_1, 0))
+        self.connect((self.osmosdr_source_0, 0), (self.ieee802_15_4_oqpsk_phy_0, 0))
+        self.connect((self.osmosdr_source_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.osmosdr_source_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "transceiver_OQPSK_sniffer")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_tx_gain(self):
+        return self.tx_gain
+
+    def set_tx_gain(self, tx_gain):
+        self.tx_gain = tx_gain
+
+    def get_rx_gain(self):
+        return self.rx_gain
+
+    def set_rx_gain(self, rx_gain):
+        self.rx_gain = rx_gain
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self._freq_callback(self.freq)
+        self.osmosdr_source_0.set_center_freq(self.freq, 0)
+
+
+
+
+def main(top_block_cls=transceiver_OQPSK_sniffer, options=None):
+    if gr.enable_realtime_scheduling() != gr.RT_OK:
+        print("Error: failed to enable real-time scheduling.")
+
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
